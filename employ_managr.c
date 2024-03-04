d00d7fd064cfea352c7ab0a84f2b6440b2f6a7fc
chandanlivo <138152782+chandanlivo@users.noreply.github.com>
2024-03-04 16:04:52 +0530

    Add files via upload

---
+++ employ_managr.c
@@ -0,0 +1,5 @@
+//DYNAMIC MEMORY ALLOCATION
+// ABC pvt ltd,,,maintains employee details of other companies
+//emp id can be of any length and can contain characters
+// for three employees you to take emp id as an input and display it on screen
+//store the emp id ina acharacter array which is allocated dynamically
