2c8b4277c2c9c000261df11b2f73336dd7de3456
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-06 13:35:07 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ float b;
+ a = 34;
+ b = 7.5;
+
+ printf("a + b = %f\n", a+b);
+ printf("a - b = %f\n", a-b);
+ printf("a * b = %f\n", a*b);
+ printf("a / b = %f\n", a/b);
+  
+}
