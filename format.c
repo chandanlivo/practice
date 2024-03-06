d2bfa7209cbef7f22e783a358f9fc2591f8d64fd
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-06 13:35:46 +0530

    Add files via upload

---
+++ format.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = 4;
+    float b = 3.333;
+    printf("The value of a os %d and the value of b id %10.4f\n", a, b);
+    printf("Heloo World");
+    return 0;
+}
